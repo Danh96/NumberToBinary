03c878db041dd1119658bdbaf12c80ff5c0b5f1d
Daniel Hayes <daniel.hayes@aviva.com>
2018-06-15 14:15:15 +0100

    Initial Commit

---
+++ NumberToBinary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberToBinary
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("please enter a whole number between 1 & 255 you wish to convert into binary");
+
+			int Entry = 0;
+			string BinaryAnswer = string.Empty;
+
+			try
+			{
+				Entry = int.Parse(Console.ReadLine());
+			}
+			catch
+			{
+				Console.WriteLine("Invalid input");
+			}
+
+			int Number = Entry;
+
+			List<int> BinaryList = new List<int> { };
+
+			while (Number >= 1)
+			{
+				int remainder = Number % 2;
+				Number = Number / 2;
+
+				BinaryAnswer = remainder + BinaryAnswer;
+			}
+
+			Console.WriteLine(BinaryAnswer);
+		}
+	}
+}
